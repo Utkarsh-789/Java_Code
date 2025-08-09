989ad9817c8e8e37032edf618b77455f707cc68d
Utkarsh Verma <155562245+Utkarsh-789@users.noreply.github.com>
2025-08-10 00:31:13 +0530

    Add files via upload

---
+++ pattern/SquareFilled.java
@@ -0,0 +1,9 @@
+public class SquareFilled {
+    public static void main(String[] args) {
+        int size = 5;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
