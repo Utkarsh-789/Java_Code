08e52bec141547e104ede825da42556210652064
Utkarsh Verma <155562245+Utkarsh-789@users.noreply.github.com>
2025-08-11 00:08:47 +0530

    Update Swap.java

--- basic/Swap.java
+++ basic/Swap.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class Swap {
     public static void main(String[] args) {
-        int a = 5, b = 10;
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first number a");
+        int a=sc.nextInt();
+        System.out.println("enter second number b");
+        int b=sc.nextInt();
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
         System.out.println("After swap: a = " + a + ", b = " + b);
     }
 }
