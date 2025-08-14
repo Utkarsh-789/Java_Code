30967c84787b357cb8adb1039dfab94549ecf92d
Utkarsh Verma <155562245+Utkarsh-789@users.noreply.github.com>
2025-08-14 23:23:46 +0530

    Update AddTwoNumbers.java

--- basic/AddTwoNumbers.java
+++ basic/AddTwoNumbers.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int a = 10, b = 20;
-        int sum = a + b;
+        int sum=a+b;
         System.out.println("Sum: " + sum);
     }
 }
