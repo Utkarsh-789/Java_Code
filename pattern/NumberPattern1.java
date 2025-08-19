d3577add200768e79917d58c3864b16f8f71af38
Utkarsh Verma <155562245+Utkarsh-789@users.noreply.github.com>
2025-08-19 23:40:34 +0530

    Update NumberPattern1.java

--- pattern/NumberPattern1.java
+++ pattern/NumberPattern1.java
@@ -1,5 +1,5 @@
 public class NumberPattern1 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
